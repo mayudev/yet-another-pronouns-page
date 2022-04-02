34924072d76e1703eafdd7b90127c6bd5793bac8
mayudev <99561108+mayudev@users.noreply.github.com>
2022-04-02 11:23:52 +0200

    chore(app): add comments

--- app/router/user.go
+++ app/router/user.go
@@ -7,7 +7,15 @@ import (
 )
 
 func AddUserRoutes(r fiber.Router) {
+	// Get user by username
 	r.Get("/user/:username", userHandler.GetUserByUsername)
+
+	// Get user by ID
 	r.Get("/id/:userID", userHandler.GetUserByID)
+
+	// Get current user
 	r.Get("/me", middleware.SessionMiddleware, userHandler.GetCurrentUser)
+
+	// Update current user
+	r.Post("/me", middleware.SessionMiddleware, userHandler.UpdateUser)
 }

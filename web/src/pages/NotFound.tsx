e3d1b18997c3cd3d0e0405107d5e2d7a863518fb
mayudev <99561108+mayudev@users.noreply.github.com>
2022-04-02 18:57:50 +0200

    feat(not-found): make it fancy

--- web/src/pages/NotFound.tsx
+++ web/src/pages/NotFound.tsx
@@ -1,13 +1,19 @@
 import "../styles/pages/NotFound.scss";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 function NotFound() {
   const navigate = useNavigate();
 
   return (
     <div className="error">
-      <div className="error__text">Page not found!</div>
+      <div className="error__header">
+        <FontAwesomeIcon icon={faExclamationTriangle} size="2x" />
+        <span className="header__title">Page not found</span>
+      </div>
       <button className="button error__button" onClick={() => navigate(-1)}>
+        <FontAwesomeIcon className="button__icon" icon={faArrowLeft} />
         Return to previous page
       </button>
     </div>

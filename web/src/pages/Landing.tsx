0375e10c3457e5ead396b9b174e653b29b2f93f0
mayudev <99561108+mayudev@users.noreply.github.com>
2022-04-05 22:38:57 +0200

    feat(web): add a simple landing page

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { LoginContext } from "../lib/context/login";
 import "../styles/pages/Landing.scss";
 
 function Landing() {
+  const [loginContext] = useContext(LoginContext);
   return (
     <div className="landing">
-      <div className="heading"></div>
+      <div className="landing__heading">a very simple display for your pronouns</div>
+      <div className="landing__subheading">you can login with discord, i suppose</div>
+      {loginContext.loggedIn ? (
+        <Link to={"/@" + loginContext.username}>
+          <button className="button landing__button">Show profile</button>
+        </Link>
+      ) : (
+        <Link to="/login">
+          <button className="button landing__button">Sign up</button>
+        </Link>
+      )}
     </div>
   );
 }
